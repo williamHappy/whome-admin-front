8ff201ab48ca12e3393528dd0448c3354f9e1b01
williamhappy_work <ctwang1994@163.com>
2018-10-12 15:57:54 +0800

    推出mock url修改

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,7 +3,7 @@ import loginAPI from './modules/login'
 
 // 登录相关
 Mock.mock(/\/user\/login/, 'post', loginAPI.loginByUsername)
-Mock.mock(/\/login\/logout/, 'post', loginAPI.logout)
+Mock.mock(/\/user\/logout/, 'post', loginAPI.logout)
 Mock.mock(/\/user\/info\.*/, 'get', loginAPI.getUserInfo)
 
 export default Mock
